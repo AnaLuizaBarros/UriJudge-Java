2b38525a2c1a84937e20848d8e391a119266b1b2
Ana Luiza <48036305+Menialinda@users.noreply.github.com>
2019-02-27 22:29:15 -0300

    Exercício 1014 do URI

---
+++ Uri1014.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg1014;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASTRA
+ */
+public class Uri1014 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        float a = in.nextFloat();
+         float b = in.nextFloat();
+         System.out.printf("%.3f",(a/b));
+         System.out.printf(" km/l");
+        
+    }
+    
+}
