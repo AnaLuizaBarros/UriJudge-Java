6a928f04c99f1fbf75d5a7c9c9c22a4ea8339a03
Ana Luiza <48036305+Menialinda@users.noreply.github.com>
2019-02-27 22:30:01 -0300

    Exercício 1016 do URI

---
+++ Uri1016.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg1016;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASTRA
+ */
+public class Uri1016 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Scanner in = new Scanner(System.in);
+       int x = in.nextInt();
+       System.out.println((x*2)+" minutos");
+    }
+    
+}
